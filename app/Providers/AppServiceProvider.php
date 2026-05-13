8d89f391529a59c22f8411b73f35c0550b4fe548
NLSilver <n.silveron.557070@umindanao.edu.ph>
2026-05-13 09:36:59 +0800

    Final Laravel Commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,27 +1,28 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Gate;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
-     * Register any application services.
-     */
+    * Register any application services.
+    */
+
     public function register(): void
     {
-        //
+    //
     }
 
     /**
-     * Bootstrap any application services.
-     */
+    * Bootstrap any application services.
+    */
+
     public function boot(): void
     {
-        Gate::define('admin', function ($user) {
-        return $user->role === 'admin';
-    });
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
